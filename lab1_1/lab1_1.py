68c2c32e85ec210f4a8c2b802b21ddaf924addf3
Anatoliy Cherevach <anatoliycherevach@gmail.com>
2021-02-21 22:03:53 +0200

    Lab 1.1

---
+++ lab1_1/lab1_1.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt  # lib for graphs
+import numpy as np  # lib for math operations
+
+# constants
+n = 12  # number of harmonics
+w = 2700  # max frequency
+N = 64  # number of descrete calls
+
+# function for calculating random signal
+def formula(a, w, t, phi):
+    return a*np.sin(w*t+phi)
+
+
+signals = [0]*N  # array of signals
+w0 = w/n  # frequency
+
+for harmonic in range(n):
+    a = np.random.rand()  # amplitude
+    phi = np.random.rand()  # phase
+
+    for t in range(N):
+        signals[t] += formula(a, w0, t, phi)
+
+    w0 += w0
+
+print('Mx:', np.average(signals))  # Average
+print('Dx:', np.var(signals))  # Dispersion
+
+# plotting 
+plt.plot(signals)
+plt.xlabel('time')
+plt.ylabel('x')
+plt.title('Random generated signals')
+plt.show()
